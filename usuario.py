845a4b9c39193a6a4471e92932d36510e4f014a9
cristianoterra <cristianoterr@gmail.com>
2023-11-30 13:34:05 -0300

    inicial

---
+++ usuario.py
@@ -0,0 +1,15 @@
+print ("Digite seu nome:")
+nome = input ()
+executar = True
+while(executar == True):
+    print("Digite seu ano de nascimento")
+    try:
+     ano = int (input () )
+     if(ano < 1922) or (ano > 2021):
+      print("O ano precisa ser entre 1922 e 2021")
+     else:
+      idade = 2022 - ano
+      print("O usuário", nome, "completou ou completará", idade, "anos de idade em 2022")
+      executar = False
+    except:
+     print("Os anos precisam ser escritos apenas com números")
